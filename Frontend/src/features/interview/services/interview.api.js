216d22891b62c4e709fdaafbbfdfb451f384b3d6
RudraMangroliya <rudramangroliya777@gmail.com>
2026-03-10 15:52:38 +0530

    forntend interview services and backend interview.controller chnage

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -25,7 +25,9 @@ export const generateInterviewReport = async ({ jobDescription, selfDescription,
     const formData = new FormData()
     formData.append("jobDescription", jobDescription)
     formData.append("selfDescription", selfDescription)
+    if (resumeFile) {
     formData.append("resume", resumeFile)
+    }
 
     const response = await api.post("/api/interview/", formData, {
         headers: {
