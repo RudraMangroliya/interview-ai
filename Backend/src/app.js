0abc80485ee35c0cf7855f1b68878692819ef58f
RudraMangroliya <rudramangroliya777@gmail.com>
2026-03-10 14:54:13 +0530

    add '/' route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,6 +21,10 @@ const interviewRouter = require("./routes/interview.routes")
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
+app.get("/", (req, res) => {
+  res.status(200).send("Server running");
+});
+
 
 
 module.exports = app
--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -5,6 +5,7 @@ const api = axios.create({
     withCredentials: true,
 })
 
+// Add a request interceptor to include the token in the Authorization header for all requests
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
 
